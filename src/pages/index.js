87edc541b1b4b20291ac0c8621a5118fa4729617
Zuhair Abbas <zuhairabs@gmail.com>
2020-04-03 00:26:41 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Contact from "src/components/Contact/Contact";
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Ahmed BESBES - Data Science Portfolio" />
+    <SEO title="Zuhair Abbas - Data Science Portfolio" />
    
     <Home />
     <About />
